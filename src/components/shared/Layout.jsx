190e0b888182edc4173cd9a114bda948f1d9be0e
antoinehage612 <hypercoffin612@gmail.com>
2023-12-23 21:49:58 +0200

    fsf

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -6,7 +6,6 @@ import Header from "./Header";
 export default function Layout() {
   return (
     <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
-      <Sidebar />
       <div className="flex-1">
         <Header />
         <div className="p-4">{<Outlet />}</div>
